96cb74aeff801967022e4e5a34b84ec876819239
J. Ackerman <github email address>
2017-07-28 23:27:48 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={alert("There's no place like 127.0.0.1")} />
   </div>,
   document.getElementById('root')
 );
